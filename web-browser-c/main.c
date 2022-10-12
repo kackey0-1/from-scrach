78d656cbbc147a66a7c589392a60770b265616ee
kackey0-1 <50774322+kackey0-1@users.noreply.github.com>
2022-10-12 21:07:36 +0900

    hello world with web-browser (#8)
    * added main.c for web-browser
    
    * fixed structure

---
+++ web-browser-c/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/errno.h>
+#include <sys/fcntl.h>
+#include <sys/mman.h>
+
+#define FB_PATH "/dev/fb0"
+#define FONT_PATH "/usr/share/fonts"
+
