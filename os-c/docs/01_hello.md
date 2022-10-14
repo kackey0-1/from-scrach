ccfbf14db1a9eed4074c479ebb1943685f307b80
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-10-14 22:23:53 +0900

    added memmap console log

---
+++ os-c/docs/01_hello.md
@@ -0,0 +1,13 @@
+# 各種ファイル説明
+
+| file name          | content       |
+|--------------------|---------------|
+| MikanLoaderPkg.dec | パッケージ宣言ファイル   |
+| MikanLoaderPkg.dsc | パッケージ記述ファイル   |
+| Loader.inf         | コンポーネント定義ファイル |
+| Main.c             | ソースコード        |
+
+## Loader.inf
+
+- ENTRY_POINTの定義
+  - UEFIアプリケーションのエントリポイント名を書く
--- os-c/workspace/oscode/MikanLoaderPkg/Main.c
+++ os-c/workspace/oscode/MikanLoaderPkg/Main.c
@@ -1,15 +1,14 @@
-#include <Uefi.h>
-#include <Library/UefiLib.h>
-#include <Library/UefiBootServicesTableLib.h>
-#include <Library/PrintLib.h>
-#include <Protocol/LoadedImage.h>
-#include <Protocol/SimpleFileSystem.h>
-#include <Protocol/DiskIo2.h>
-#include <Protocol/BlockIo.h>
+#include  <Uefi.h>
+#include  <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Library/PrintLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Protocol/DiskIo2.h>
+#include  <Protocol/BlockIo.h>
 
 // #@@range_begin(struct_memory_map)
 struct MemoryMap {
-  // memory structure
   UINTN buffer_size;
   VOID* buffer;
   UINTN map_size;
@@ -64,20 +63,30 @@ EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file) {
   CHAR8 buf[256];
   UINTN len;
 
-  CHAR8* header = 
+  CHAR8* header =
     "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
   len = AsciiStrLen(header);
   file->Write(file, &len, header);
 
+  Print(L"map->buffer = %08lx, map->map_size = %08lx\n",
+      map->buffer, map->map_size);
+
   EFI_PHYSICAL_ADDRESS iter;
   int i;
   for (iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i = 0;
-      iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
-      iter += map->descriptor_size, i++) {
+       iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
+       iter += map->descriptor_size, i++) {
     EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)iter;
+
+    Print("%u, %x, %-ls, %08lx, %lx, %lx\n",
+        i, desc->Type, GetMemoryTypeUnicode(desc->Type),
+        desc->PhysicalStart, desc->NumberOfPages,
+        desc->Attribute & 0xffffflu);
+
+
     len = AsciiSPrint(
         buf, sizeof(buf),
-        "%u, %x-ls, %08lx, %lx, %ln\n",
+        "%u, %x, %-ls, %08lx, %lx, %lx\n",
         i, desc->Type, GetMemoryTypeUnicode(desc->Type),
         desc->PhysicalStart, desc->NumberOfPages,
         desc->Attribute & 0xffffflu);
@@ -108,12 +117,15 @@ EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root) {
       NULL,
       EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
 
+  fs->OpenVolume(fs, root);
+
   return EFI_SUCCESS;
 }
 
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
     EFI_SYSTEM_TABLE* system_table) {
+  Print(L"Hello, Mikan World!\n");
 
   // #@@range_begin(main)
   CHAR8 memmap_buf[4096 * 4];
@@ -134,29 +146,7 @@ EFI_STATUS EFIAPI UefiMain(
 
   Print(L"All done\n");
 
-  while(1);
+  while (1);
   return EFI_SUCCESS;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
