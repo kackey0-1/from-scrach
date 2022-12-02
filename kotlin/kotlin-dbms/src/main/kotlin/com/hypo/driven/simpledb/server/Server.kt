824eb5ba0e77f798c72c9fe9a0cb9cc6c013bc65
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-12-02 09:39:41 +0900

    added private methods

--- kotlin/kotlin-dbms/src/main/kotlin/com/hypo/driven/simpledb/server/Server.kt
+++ kotlin/kotlin-dbms/src/main/kotlin/com/hypo/driven/simpledb/server/Server.kt
@@ -12,50 +12,31 @@ import java.util.Scanner
 
 fun main(args: Array<String>) {
     val sc = Scanner(System.`in`)
-    println("Connect> ")
+    println()
+    print("Connect> ")
     val s = sc.nextLine()
     val driver = EmbeddedDriver()
     try {
         driver.connect(s, null).also { conn ->
             val stmt = conn.createStatement()
-
-            val drop = "DROP TABLE STUDENT"
-            stmt.executeUpdate(drop)
-
-            val create = "CREATE TABLE STUDENT(SId int, SName VARCHAR(10), MajorId int, GradYear int)"
-            stmt.executeUpdate(create)
-            println("Created table STUDENT")
-
-            val insert = "INSERT INTO STUDENT VALUES"
-            val students = arrayOf(
-                "(1, 'Zhang', 10, 2004)",
-                "(2, 'Wang', 20, 2005)",
-                "(3, 'Li', 10, 2004)",
-                "(4, 'Cheng', 20, 2005)",
-                "(5, 'He', 30, 2004)",
-                "(6, 'Wu', 10, 2004)",
-                "(7, 'Zhao', 10, 2004)",
-                "(8, 'Qian', 20, 2005)",
-                "(9, 'Sun', 30, 2004)",
-                "(10, 'Liu', 20, 2004)",
-                "(11, 'Feng', 30, 2005)"
-            )
-            for (s in students)
-                stmt.executeUpdate(insert + s)
-            println("STUDENT records created")
-
-            val update = "UPDATE STUDENT SET GradYear = 2005 WHERE SName = 'He'"
-            stmt.executeUpdate(update)
-            println("STUDENT records updated")
-
-            val query = "SELECT SName, MajorId, GradYear FROM STUDENT"
-            val rs = stmt.executeQuery(query)
-            printSchema(rs)
-            while (rs.next()) {
-                val sName = rs.getString("SName")
-                val majorId = rs.getInt("MajorId")
-                val gradYear = rs.getInt("GradYear")
-                println("Name: $sName, MajorId: $majorId, GradYear: $gradYear")
+            init(stmt)
+            println()
+            print("SQL> ")
+            while (sc.hasNextLine()) {
+                // process one line of input
+                val cmd = sc.nextLine().trim().lowercase()
+                if (cmd.startsWith("exit"))
+                    break
+                else if (cmd.startsWith("select"))
+                    doQuery(stmt, cmd)
+                else if (cmd.startsWith("insert"))
+                    doUpdate(stmt, cmd)
+                else if (cmd.startsWith("update"))
+                    doUpdate(stmt, cmd)
+                else
+                    println("invalid command")
+                println()
+                print("SQL> ")
             }
 
             conn.close()
@@ -63,6 +44,35 @@ fun main(args: Array<String>) {
 
     } catch (e: SQLException) {
         println("database connection failed")
+        e.printStackTrace()
+    }
+}
+
+private fun init(stmt: Statement) {
+    try {
+        val drop = "DROP TABLE STUDENT"
+        stmt.executeUpdate(drop)
+        val create = "CREATE TABLE STUDENT(SId int, SName VARCHAR(10), MajorId int, GradYear int)"
+        stmt.executeUpdate(create)
+        println("Created table STUDENT")
+        val insert = "INSERT INTO STUDENT VALUES"
+        val students = arrayOf(
+            "(1, 'Zhang', 10, 2004)",
+            "(2, 'Wang', 20, 2005)",
+            "(3, 'Li', 10, 2004)",
+            "(4, 'Cheng', 20, 2005)",
+            "(5, 'He', 30, 2004)",
+            "(6, 'Wu', 10, 2004)",
+            "(7, 'Zhao', 10, 2004)",
+            "(8, 'Qian', 20, 2005)",
+            "(9, 'Sun', 30, 2004)",
+            "(10, 'Liu', 20, 2004)",
+            "(11, 'Feng', 30, 2005)"
+        )
+        for (s in students)
+            stmt.executeUpdate(insert + s)
+        println("Inserted " + students.size + " rows into STUDENT")
+    } catch (e: SQLException) {
         e.printStackTrace()
     }
 }
@@ -93,7 +103,7 @@ private fun doQuery(stmt: Statement, cmd: String) {
                 val width = md.getColumnDisplaySize(i)
                 totalWidth += width
                 val fmt = "%${width}s"
-                println(String.format(fmt, fieldName))
+                print(String.format(fmt, fieldName))
             }
             println()
             for (i in 0..totalWidth) print("-")
@@ -109,16 +119,15 @@ private fun doQuery(stmt: Statement, cmd: String) {
                     when (fieldType) {
                         Types.INTEGER -> {
                             val ival = rs.getInt(fieldName)
-                            println(String.format(fmt, ival))
+                            print(String.format(fmt, ival))
                         }
                         else -> {
                             val sval = rs.getString(fieldName)
-                            println(String.format(fmt, sval))
+                            print(String.format(fmt, sval))
                         }
                     }
-                        val value = rs.getString(i)
-                    println(String.format(fmt, value))
                 }
+                println()
             }
         }
     } catch (e: SQLException) {
